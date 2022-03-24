87278f28fd186485f430c1a44477dee875b0b8da
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-24 16:20:12 -0300

    Dividir a string de data

--- PeriodosAtras.ConsoleApp/Program.cs
+++ PeriodosAtras.ConsoleApp/Program.cs
@@ -6,7 +6,24 @@ namespace PeriodosAtras.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Data de hoje: " + DateTime.Now.ToShortDateString());
+
+            Console.Write("Digite uma data (dd/mm/YYYY): ");
+            string data_str = Console.ReadLine();
+
+            string[] data_array = data_str.Split('/');
+            string dias_array = data_array[0];
+            string meses_array = data_array[1];
+            string anos_array = data_array[2];
+
+            int dias = Convert.ToInt32(dias_array);
+            int meses = Convert.ToInt32(meses_array);
+            int anos = Convert.ToInt32(anos_array);
+
+            if (dias <= 31 && meses <= 12 && anos <= 2022)
+            {
+                Console.WriteLine("{0}/{1}/{2}", dias, meses, anos);
+            }
         }
     }
 }
