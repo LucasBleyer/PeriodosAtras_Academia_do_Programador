4ebd195d060dd8aa4b84c065112db313e298cc80
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-24 16:40:32 -0300

    mostrando valores e verificando se a data esta correta

--- PeriodosAtras.ConsoleApp/Program.cs
+++ PeriodosAtras.ConsoleApp/Program.cs
@@ -8,21 +8,32 @@ namespace PeriodosAtras.ConsoleApp
         {
             Console.WriteLine("Data de hoje: " + DateTime.Now.ToShortDateString());
 
-            Console.Write("Digite uma data (dd/mm/YYYY): ");
-            string data_str = Console.ReadLine();
-
-            string[] data_array = data_str.Split('/');
-            string dias_array = data_array[0];
-            string meses_array = data_array[1];
-            string anos_array = data_array[2];
-
-            int dias = Convert.ToInt32(dias_array);
-            int meses = Convert.ToInt32(meses_array);
-            int anos = Convert.ToInt32(anos_array);
-
-            if (dias <= 31 && meses <= 12 && anos <= 2022)
+            int dias, meses, anos;
+            while (true)
             {
-                Console.WriteLine("{0}/{1}/{2}", dias, meses, anos);
+                Console.Write("Digite uma data (dd/mm/YYYY): ");
+                string data_str = Console.ReadLine();
+
+                string[] data_array = data_str.Split('/');
+                string dias_array = data_array[0];
+                string meses_array = data_array[1];
+                string anos_array = data_array[2];
+
+                dias = Convert.ToInt32(dias_array);
+                meses = Convert.ToInt32(meses_array);
+                anos = Convert.ToInt32(anos_array);
+
+                if (dias <= 31 && meses <= 12 && anos <= 2022)
+                {
+                    Console.WriteLine("Data informada: {0}/{1}/{2}", dias, meses, anos);
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Formato incorreto, digite novamente...\n");
+                    Console.ResetColor();
+                }
             }
         }
     }
